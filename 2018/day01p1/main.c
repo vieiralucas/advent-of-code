47f30b1abc3f54148d3c672df6ca7d3ab4b7c9f0
Lucas Vieira <vieiralucas4@gmail.com>
2018-12-12 01:31:44 -0200

    2018 day 1 part 1

---
+++ 2018/day01p1/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() 
+{
+  FILE *fd = fopen("input.txt", "r");
+  if (fd == NULL) {
+    printf("Failed to open input.txt");
+    fclose(fd);
+    return 1;
+  }
+
+  char *line = NULL;
+  size_t cap = 0;
+  int sum = 0;
+  while (true) {
+    int end = getline(&line, &cap, fd);
+    if (end == -1) {
+      break;
+    }
+
+    int i;
+    char signal;
+    int err = sscanf(line, "%c%d\n", &signal, &i);
+    if (err < 0) {
+      printf("Failed to read line: %s", line);
+      goto fail;
+    }
+
+    if (signal == '+') {
+      sum += i;
+    } else if (signal == '-') {
+      sum -= i;
+    } else {
+      printf("Unexpected signal %c", signal);
+      goto fail;
+    }
+  }
+
+  printf("%d\n", sum);
+  return 0;
+
+fail:
+  fclose(fd);
+  free(line);
+  return 1;
+}
